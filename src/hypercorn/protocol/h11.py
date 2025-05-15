1ed04946b3dbd2bf18dc521bf5bcfb56759080bc
Maxim Kasimov <kasimov.maxim@gmail.com>
2025-05-15 16:30:03 +0300

    Send  response's reason

--- src/hypercorn/protocol/h11.py
+++ src/hypercorn/protocol/h11.py
@@ -25,6 +25,80 @@ from ..typing import AppWrapper, ConnectionState, H11SendableEvent, TaskGroup, W
 STREAM_ID = 1
 
 
+HTTP_REASON_PHRASES = {
+    100: "Continue",
+    101: "Switching Protocols",
+    102: "Processing",  # WebDAV
+    103: "Early Hints",  # HTTP/1.1 + Link preload
+
+    200: "OKKKKK",
+    201: "Created",
+    202: "Accepted",
+    203: "Non-Authoritative Information",
+    204: "No Content",
+    205: "Reset Content",
+    206: "Partial Content",
+    207: "Multi-Status",  # WebDAV
+    208: "Already Reported",  # WebDAV
+    226: "IM Used",  # Delta encoding
+
+    300: "Multiple Choices",
+    301: "Moved Permanently",
+    302: "Found",
+    303: "See Other",
+    304: "Not Modified",
+    305: "Use Proxy",
+    307: "Temporary Redirect",
+    308: "Permanent Redirect",
+
+    400: "Bad Request",
+    401: "Unauthorized",
+    402: "Payment Required",
+    403: "Forbidden",
+    404: "Not Found",
+    405: "Method Not Allowed",
+    406: "Not Acceptable",
+    407: "Proxy Authentication Required",
+    408: "Request Timeout",
+    409: "Conflict",
+    410: "Gone",
+    411: "Length Required",
+    412: "Precondition Failed",
+    413: "Payload Too Large",
+    414: "URI Too Long",
+    415: "Unsupported Media Type",
+    416: "Range Not Satisfiable",
+    417: "Expectation Failed",
+    418: "I'm a teapot",  # Easter egg (RFC 2324)
+    421: "Misdirected Request",
+    422: "Unprocessable Entity",  # WebDAV
+    423: "Locked",  # WebDAV
+    424: "Failed Dependency",  # WebDAV
+    425: "Too Early",
+    426: "Upgrade Required",
+    428: "Precondition Required",
+    429: "Too Many Requests",
+    431: "Request Header Fields Too Large",
+    451: "Unavailable For Legal Reasons",
+
+    500: "Internal Server Error",
+    501: "Not Implemented",
+    502: "Bad Gateway",
+    503: "Service Unavailable",
+    504: "Gateway Timeout",
+    505: "HTTP Version Not Supported",
+    506: "Variant Also Negotiates",
+    507: "Insufficient Storage",  # WebDAV
+    508: "Loop Detected",  # WebDAV
+    510: "Not Extended",
+    511: "Network Authentication Required",
+}
+
+
+def get_reason_phrase(status_code: int) -> str:
+    return HTTP_REASON_PHRASES.get(status_code, "")
+
+
 class H2CProtocolRequiredError(Exception):
     def __init__(self, data: bytes, request: h11.Request) -> None:
         settings = ""
@@ -127,6 +201,7 @@ class H11Protocol:
                     h11.Response(
                         headers=headers,
                         status_code=event.status_code,
+                        reason=get_reason_phrase(event.status_code),
                     )
                 )
             else:
@@ -134,6 +209,7 @@ class H11Protocol:
                     h11.InformationalResponse(
                         headers=list(chain(event.headers, self.config.response_headers("h11"))),
                         status_code=event.status_code,
+                        reason=get_reason_phrase(event.status_code),
                     )
                 )
         elif isinstance(event, InformationalResponse):
